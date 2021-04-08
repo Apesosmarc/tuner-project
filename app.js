47f058f5cf8d195e6ac17fd11e606aa312e82e8e
Marc Apesos <apesos2013@gmail.com>
2021-04-08 16:41:30 -0400

    shortened code using destructuring and loops

--- app.js
+++ app.js
@@ -1,45 +1,26 @@
-const e = document.querySelector("#e-sound");
-const a = document.querySelector("#a-sound");
-const d = document.querySelector("#d-sound");
-const g = document.querySelector("#g-sound");
+// E1 = Low E on 1st String
+// A2 = Low A on 2nd String
+// D3 = Low d 3rd string...
 
-const e1 = document.querySelector("#e1");
-const a2 = document.querySelector("#a2");
-const d3 = document.querySelector("#d3");
-const g4 = document.querySelector("#g4");
-
+const audioFiles = [...document.querySelectorAll("audio")];
+const [e1, a2, d3, g4] = audioFiles;
 const playBtns = [...document.querySelectorAll(".play-btn")];
+
+audioFiles.forEach((file) => (file.loop = "true"));
 
 for (let btn of playBtns) {
   btn.addEventListener("click", function () {
     if (btn.id === "e-sound") {
-      e1.loop = "true";
       togglePlay(e1);
     } else if (btn.id === "a-sound") {
-      a2.loop = "true";
       togglePlay(a2);
     } else if (btn.id === "d-sound") {
-      d3.loop = "true";
       togglePlay(d3);
     } else if (btn.id === "g-sound") {
-      g4.loop = "true";
       togglePlay(g4);
     }
   });
 }
-
-// e.addEventListener("click", function () {
-//   e.playAudio();
-// });
-// e.addEventListener("click", function () {
-//   e.playAudio();
-// });
-// e.addEventListener("click", function () {
-//   e.playAudio();
-// });
-// e.addEventListener("click", function () {
-//   e.playAudio();
-// });
 
 function togglePlay(audio) {
   return audio.paused ? audio.play() : audio.pause();
